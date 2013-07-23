ae0d3f773703afacc6c42eac75120d6b907582df
Ben McKibben <bmckibbeniii@gmail.com>
2013-07-23 17:49:48 -0400

    converting LL text

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Literallight::Application.config.secret_key_base = 'd2a77193f6f843fcb72c6649f91746f181fff02d84ebe5b0cdd88fadc8d5ff21003c48b241fd379e60353af3552399377a93952a19d08a81f377fc0c45875900'
---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'SETTING UP DEFAULT USER LOGIN'
+user = User.create! :name => 'Boyd-z', :email => 'bmc@literallight.com', :password => 'wm1as0De', :password_confirmation => 'wm1as0De'
+puts 'New user created: ' << user.name
+user2 = User.create! :name => 'Guest', :email => 'you@email.com', :password => 'password', :password_confirmation => 'password'
+puts 'New user created: ' << user2.name
