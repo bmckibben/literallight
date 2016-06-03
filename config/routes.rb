86e5adaeddd41d130cc0caba31f5a0618f2080e8
bbmckibb <bbmckibb@iu.edu>
2016-06-03 12:41:42 -0400

    added journal, not tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
 
+    resources :journals
 	resources :nominees
   	devise_for :users
   	resources :users
 
 	root 'home#home'
 	
-	get 'static_pages/about', :as => 'static_about'
-	get 'static_pages/help', :as => 'static_help'
-	get 'static_pages/contact', :as => 'static_contact'
-	get 'static_pages/startrek', :as => 'static_star_trek'
-	get 'static_pages/sysiphus', :as => 'static_sysiphus'
-	get 'static_pages/novels', :as => 'static_novels'
-	get 'home/home', :as => 'home'
-	get 'angular_test/index', :as => 'angular_test'
-  	get "users/list", :as => 'users_list'
+	get 'static_pages/about' => 'static_pages#about'
+	get 'static_pages/help' => 'static_pages#help'
+	get 'static_pages/contact' => 'static_pages#contact'
+	get 'static_pages/startrek' => 'static_pages#star_trek'
+	get 'static_pages/sysiphus' => 'static_pages#sysiphus'
+	get 'static_pages/moderation' => 'static_pages#moderation'
+	get 'static_pages/novels' => 'static_pages#novels'
+	get 'home/home' => 'home'
+	get 'angular_test/index' => 'angular_test'
+  	get 'users/list' => 'users#list'
+  	get 'journals/musings' => 'journals#musings', as journals_musings
   	
 end
