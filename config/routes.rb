8de537e520cb1e0c987eac34d38266892b296593
bbmckibb <bbmckibb@iu.edu>
2016-06-03 13:16:49 -0400

    fix path error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,17 +7,16 @@ Rails.application.routes.draw do
 
 	root 'home#home'
 	
-	get 'home/home' => 'home'
-	get 'static_pages/about' => 'static_pages#about'
-	get 'static_pages/help' => 'static_pages#help'
-	get 'static_pages/contact' => 'static_pages#contact'
-	get 'static_pages/startrek' => 'static_pages#star_trek'
-	get 'static_pages/sysiphus' => 'static_pages#sysiphus'
-	get 'static_pages/moderation' => 'static_pages#moderation'
-	get 'static_pages/novels' => 'static_pages#novels'
-	get 'home/home' => 'home'
-	get 'angular_test/index' => 'angular_test'
-  	get 'users/list' => 'users#list'
-  	get 'journals/musings' => 'journals#musings'
+	get 'home/home' => 'home', :as => 'home'
+	get 'static_pages/about' => 'static_pages#about', :as => 'static_pages_about'
+	get 'static_pages/help' => 'static_pages#help', :as => 'static_pages_help'
+	get 'static_pages/contact' => 'static_pages#contact', :as => 'static_pages_contact'
+	get 'static_pages/startrek' => 'static_pages#star_trek', :as => 'static_pages_startrek'
+	get 'static_pages/sysiphus' => 'static_pages#sysiphus', :as => 'static_pages_sysiphus'
+	get 'static_pages/moderation' => 'static_pages#moderation', :as => 'static_pages_moderation'
+	get 'static_pages/novels' => 'static_pages#novels', :as => 'static_pages_novels'
+	get 'angular_test/index' => 'angular_test', :as => 'angular_test_index'
+  	get 'users/list' => 'users#list', :as => 'users_list'
+  	get 'journals/musings' => 'journals#musings', :as => 'journals_musings'
   	
 end
