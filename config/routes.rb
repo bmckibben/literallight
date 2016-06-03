293107bfa2fc9a0fd1680363b5c281c153f46487
bbmckibb <bbmckibb@iu.edu>
2016-06-03 13:29:33 -0400

    fix path error in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 	get 'static_pages/about' => 'static_pages#about', :as => 'static_about'
 	get 'static_pages/help' => 'static_pages#help', :as => 'static_help'
 	get 'static_pages/contact' => 'static_pages#contact', :as => 'static_contact'
-	get 'static_pages/startrek' => 'static_pages#star_trek', :as => 'static_startrek'
+	get 'static_pages/startrek' => 'static_pages#star_trek', :as => 'static_star_trek'
 	get 'static_pages/sysiphus' => 'static_pages#sysiphus', :as => 'static_sysiphus'
 	get 'static_pages/moderation' => 'static_pages#moderation', :as => 'static_moderation'
 	get 'angular_test/index' => 'angular_test', :as => 'angular_test'
