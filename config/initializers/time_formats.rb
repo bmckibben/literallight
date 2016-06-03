46d143985b8f54219f455642df9d102a2c5dea81
bbmckibb <bbmckibb@iu.edu>
2016-06-03 13:02:50 -0400

    fix typo in time_formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
-time::DATE_FORMATS[:date] = "%d"
-time::DATE_FORMATS[:month_year] = "%B %Y"
-time::DATE_FORMATS[:day_of_week] = "%A"
-time::DATE_FORMATS[:stardate] = "%Y.%j"
+Time::DATE_FORMATS[:date] = "%d"
+Time::DATE_FORMATS[:month_year] = "%B %Y"
+Time::DATE_FORMATS[:day_of_week] = "%A"
+Time::DATE_FORMATS[:stardate] = "%Y.%j"
