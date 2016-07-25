39f87898b46bb0ea3a8647d6c4c96886be195abe
bbmckibb <bbmckibb@iu.edu>
2016-07-25 13:08:02 -0400

    angular search step 1

--- app/assets/javascripts/angular_test.js
+++ app/assets/javascripts/angular_test.js
@@ -1,1 +1,16 @@
 angular.module('angular_test',[ ]);
+
+var app = angular.module('musings',[]);
+
+var MusingSearchController = function($scope) {
+	$scope.search = function(searchTerm){
+		$scope.searchedFor = searchTerm;
+	}
+}
+
+app.controller("MusingSearchController", ["$scope"],
+	function($scope){
+		$scope.search= function(searchTerm) {
+			$scope.searchedFor = searchTerm;
+		}
+	})
