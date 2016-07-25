f43c63ad2d8d280e8f61f98718f0bbff6b5bb95c
bbmckibb <bbmckibb@iu.edu>
2016-07-25 12:55:18 -0400

    angular search step 1

--- app/assets/javascripts/musings_app.js
+++ app/assets/javascripts/musings_app.js
@@ -1,5 +1,10 @@
 var app = angular.module('musings',[]);
 
+var MusingSearchController = function($scope) {
+	$scope.search = function(searchTerm){
+		$scope.searchedFor = searchTerm;
+	}
+}
 
 app.controller("MusingSearchController", ["$scope"],
 	function($scope){
