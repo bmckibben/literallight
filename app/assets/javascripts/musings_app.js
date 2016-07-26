bf0e489c1760f37018c9ddfa4a84d202d2a488b0
bbmckibb <bbmckibb@iu.edu>
2016-07-26 16:03:45 -0400

    meh

--- app/assets/javascripts/musings_app.js
+++ app/assets/javascripts/musings_app.js
@@ -3,7 +3,7 @@ var appMusing = angular.module('apMusingSearch', []);
 appMusing.controller("ctlrMusingSearch", [
 	'$scope',
 	function($scope) {
-		$scope.greetting = "Dude!";
+		$scope.greeting = "Dude!";
 		$scope.search = function(searchTerm) {
 			$scope.searchedFor = searchTerm;
 		}
