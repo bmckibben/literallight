a48f091cdacce8cc235ab4fbc5da183bcc8beea6
bbmckibb <bbmckibb@iu.edu>
2016-07-27 14:17:26 -0400

    moving musings to journal controller

--- app/assets/javascripts/musings_app.js
+++ app/assets/javascripts/musings_app.js
@@ -6,7 +6,7 @@ appMusing.controller("ctlrMusingSearch", [
 		$scope.greeting = "Search Musings";
 		$scope.musings = [];
 		$scope.search = function(searchTerm) {
-			$http.get("/journals/musing.json",
+			$http.get("/musing.json",
 				{"params": {"searchwords" : searchTerm}}
 				).success(
 					function(data,status,headers,config){
