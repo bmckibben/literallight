425b6c37bda1c7b9c2f8d0f6d7dc3fcb3d390620
bbmckibb <bbmckibb@iu.edu>
2016-07-27 09:10:18 -0400

    building display loop with dummy data

--- app/assets/javascripts/musings_app.js
+++ app/assets/javascripts/musings_app.js
@@ -7,16 +7,17 @@ appMusing.controller("ctlrMusingSearch", [
 		$scope.musings = [];
 		$scope.search = function(searchTerm) {
 			$scope.musings = [
-			{"entry_for" : "2016-01-02"
-			 ,"note" : "test  note january"
-			}
-		   ,{"entry_for" : "2016-02-02"
-			,"note" : "test  note february"
-			}
-			,{"entry_for" : "2016-02-02"
-			,"note" : "test  note march"
+			{"entry_for" : "2016-01-02",
+			 "note" : "test  note january"
+			},
+		    {"entry_for" : "2016-02-02",
+			"note" : "test  note february"
+			},
+			{"entry_for" : "2016-02-02",
+			"note" : "test  note march"
 			}	
-			]
+			],
+			$scope.searchedFor = searchTerm;
 		};
 	}
 ])
