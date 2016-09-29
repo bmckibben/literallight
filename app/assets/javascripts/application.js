559865d3332ce4da6582e258333e3cd30a96c5c8
bbmckibb <bbmckibb@iu.edu>
2016-09-29 12:38:28 -0400

    jquery bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sass/assets/javascripts/bootstrap-sprockets
+//= require bootstrap-sass-official/assets/javascripts/bootstrap-sprockets
 //= require angular
 //= require_tree .
