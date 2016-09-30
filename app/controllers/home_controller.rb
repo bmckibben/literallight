38bdb3c20dafa4d33e9547680e9ca8332aa365a5
Ben McKibben <bbmckibb@iu.edu>
2016-09-30 14:30:14 -0400

    creating mailer

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
   def index
   	render layout: false
   end
+  
   def home
   	
   end  
+
+  def contact_email
+  	HomeMailer.send_contact_email(email_name,email_address,email_message).deliver_now
+  end	
 end
