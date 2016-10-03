c85a5c154cf4cdc9a52946d71f003b5903d70678
Ben McKibben <bbmckibb@iu.edu>
2016-10-03 15:07:12 -0400

    fixed closing quote

--- app/mailers/home_mailer.rb
+++ app/mailers/home_mailer.rb
@@ -2,7 +2,7 @@ class HomeMailer < ApplicationMailer
 
   def send_contact_email(email_name,email_address,email_message)
 
-      mail(to: "mckibben@att.net", subject: "Contact from LiteralLight)
+      mail(to: "mckibben@att.net", subject: "Contact from LiteralLight")
 
   end
 
