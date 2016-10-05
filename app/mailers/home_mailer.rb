54410199d08729c764daad18e48a4a38cf7a94c4
Ben McKibben <mckibben@att.net>
2016-10-04 20:56:56 -0400

    mail issue

--- app/mailers/home_mailer.rb
+++ app/mailers/home_mailer.rb
@@ -2,7 +2,11 @@ class HomeMailer < ApplicationMailer
 
   def send_contact_email(email_name,email_address,email_message)
 
-      mail(to: "mckibben@att.net", subject: "Contact from LiteralLight")
+  	@email_name = email_name
+  	@email_address = email_address
+  	@email_message = email_message
+
+    mail(to: "mckibben@att.net", subject: "Contact from LiteralLight")
 
   end
 
